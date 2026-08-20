ae02e3aaa35044a6c8cf84c66bdcfda06f19c768
Guilherme Shimada <118783195+GuiShimada@users.noreply.github.com>
2026-08-19 22:45:41 -0300

    Adicionada classe Department

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,13 @@
 package aplication;
 
 import db.DB;
+import model.entities.Department;
 
 import java.sql.Connection;
 
 public class Program {
-
-    Connection connection = DB.getConnection();
-
+    public static void main(String[] args) {
+        Department department = new Department(1, "Guilherme");
+        System.out.println(department);
+    }
 }
---
+++ src/model/entities/Department.java
@@ -0,0 +1,51 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private int id;
+    private String name;
+
+    public static final long serialVersionUID = 1L;
+
+    public Department() {}
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(id).append("- ").append(name);
+        return sb.toString();
+    }
+
+}
